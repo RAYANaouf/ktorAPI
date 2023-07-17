41cf7411c3e4ff4f208b8c93f0894a6c161de852
rayan_aouf <rayanaouf1512@gmail.com>
2023-07-17 15:41:12 +0100

    ktor_api_v0.1.2

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -10,7 +10,13 @@ import org.ktorm.database.Database
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0"){
-//        var database = Database.connect(url = "jdbc:mysql://aws.connect.psdb.cloud/tryingv1?sslMode=VERIFY_IDENTITY", user = "3jecm085g6dhpwvc9wv7" , password = "pscale_pw_xvmkSHGJax6tGvRt3zhBzpeFg36L8QBjiIpcwBiTaXe")
+
+        Database.connect(
+            url = "jdbc:mysql://aws.connect.psdb.cloud/tryingv1",
+            driver = "com.mysql.jdbc.Driver",
+            user = "07dk6aba8gvplpby8fhq",
+            password = "pscale_pw_3HZ8xYKc35a4wJCEgLindUiYvZMQTyz5CieSMeizoH"
+        )
 
         routing {
             get("/") {
